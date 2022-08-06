3f476b6ba3dab6d11574d0b9e8bf318f858f1e1f
seeker <majijun108@sina.com>
2022-08-06 16:15:15 +0800

    story提交
    Signed-off-by: seeker <majijun108@sina.com>

---
+++ Story/Scripts/Nodes/StoryBaseNode.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace Story
+{
+    public abstract class StoryBaseNode : Node
+    {
+        
+    }
+}
---
+++ Story/Scripts/Nodes/TalkNode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+namespace Story
+{
+    [CreateNodeMenu("对话")]
+    [NodeWidth(200)]
+    [NodeTitle("对话")]
+    public class TalkNode:StoryBaseNode
+    {
+        [Input]
+        public StoryBaseNode input;
+        [Output(backingValue = ShowBackingValue.Never,connectionType = ConnectionType.Override,dynamicPortList =true)]
+        public List<ChatData> chats = new List<ChatData>();
+    }
+
+    [System.Serializable]
+    public class ChatData 
+    {
+        public string Name;
+        public string Content;
+    }
+}
